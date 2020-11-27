7d7d25feedf35ac5255170a842d2a2d9dd2c5d06
nekomehako <ngnlkkngsm@yahoo.co.jp>
2020-11-27 17:12:09 +0900

    set Uniform variables

--- script.js
+++ script.js
@@ -1,27 +1,50 @@
 onload = function(){
     // canvasエレメントを取得
     const c = document.getElementById('canvas');
-    c.width = 500;
-    c.height = 300;
+    const CANVAS_SIZE_X = 600.0;
+    const CANVAS_SIZE_Y = 300.0;
+
+    c.width = CANVAS_SIZE_X;
+    c.height = CANVAS_SIZE_Y;
 
     // webglコンテキストを取得
-    gl = c.getContext('webgl2')
+    const gl = c.getContext('webgl2')
     // canvasを黒でクリア(初期化)する
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    const v_shader = create_shader("vs");
-    const f_shader = create_shader("fs");
+    const v_shader = create_shader(gl,"vs");
+    const f_shader = create_shader(gl,"fs");
     
     // プログラムオブジェクトの生成とリンク
-    const prg = create_program(v_shader, f_shader);
+    const prg = create_program(gl,v_shader, f_shader);
     
+    gl.uniform2f(gl.getUniformLocation(prg, 'resolution'), CANVAS_SIZE_X, CANVAS_SIZE_Y);
+
+    //texture
+    const framebuffer = gl.createFramebuffer();
+    gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffer)
+    const velocityTexture = createTexture(gl, CANVAS_SIZE_X, CANVAS_SIZE_Y, gl.RG32F, gl.RG, gl.FLOAT)
+    gl.bindTexture(gl.TEXTURE_2D, velocityTexture);
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, velocityTexture, 0);
+
+    const dencityTexture = createTexture(gl, CANVAS_SIZE_X, CANVAS_SIZE_Y, gl.R32F, gl.RED, gl.FLOAT)
+    gl.bindTexture(gl.TEXTURE_2D, dencityTexture);
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT1, gl.TEXTURE_2D, dencityTexture, 0);
+
+    gl.drawBuffers([gl.COLOR_ATTACHMENT0, gl.COLOR_ATTACHMENT1]);
+
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+    gl.bindTexture(gl.TEXTURE_2D, null);
+
+    const velocityLocation = gl.getUniformLocation(prg, 'u_velocity');
+    setUniformTexture(gl, velocityLocation, velocityTexture);
+
+    const dencityLocation = gl.getUniformLocation(prg, 'u_dencity');
+    setUniformTexture(gl, dencityLocation, dencityTexture);
+
     // vLocationの取得
     const VERTEX_SIZE = 3
-    const COLOR_SIZE = 4
-    const STRIDE = (VERTEX_SIZE + COLOR_SIZE) * Float32Array.BYTES_PER_ELEMENT;
-    const POSITION_OFFSET = 0;
-    const COLOR_OFFSET = VERTEX_SIZE * Float32Array.BYTES_PER_ELEMENT;
 
     const vertexBuffer = gl.createBuffer();
     
@@ -30,16 +53,12 @@ onload = function(){
 
     const vAttribLocation = gl.getAttribLocation(prg, 'vertexPosition');
     gl.enableVertexAttribArray(vAttribLocation);
-    gl.vertexAttribPointer(vAttribLocation, VERTEX_SIZE, gl.FLOAT, false, STRIDE, POSITION_OFFSET);
+    gl.vertexAttribPointer(vAttribLocation, VERTEX_SIZE, gl.FLOAT, false, 0, 0);
     const vertices = new Float32Array([
         -1.0, 1.0,  0.0,      // xyz
-        1.0,  0.0,  0.0, 1.0, // rgba
         -1.0, -1.0, 0.0,
-        0.0,  1.0,  0.0, 1.0,
         1.0,  1.0,  0.0,
-        0.0,  0.0,  1.0, 1.0,
         1.0,  -1.0, 0.0,
-        0.0,  0.0,  0.0, 1.0
     ]);
 
     const indices = new Uint16Array([
@@ -47,9 +66,6 @@ onload = function(){
         1, 3, 2
     ]);
 
-    const colorAttribLocation = gl.getAttribLocation(prg, 'color');
-    gl.enableVertexAttribArray(colorAttribLocation);
-    gl.vertexAttribPointer(colorAttribLocation, COLOR_SIZE, gl.FLOAT, false, STRIDE, COLOR_OFFSET);
 
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
@@ -60,10 +76,10 @@ onload = function(){
     const indexSize = indices.length;
     gl.drawElements(gl.TRIANGLES, indexSize, gl.UNSIGNED_SHORT, 0);
 
-
+    gl.flush(); 
 };
 
-function create_shader(id){
+function create_shader(gl, id){
     // シェーダを格納する変数
     let shader;
     // HTMLからscriptタグへの参照を取得
@@ -73,12 +89,12 @@ function create_shader(id){
     // scriptタグのtype属性をチェック
     switch(scriptElement.type){
         // 頂点シェーダの場合
-        case 'x-shader/x-vertex':
+        case 'vertex-shader':
             shader = gl.createShader(gl.VERTEX_SHADER);
             break;
             
         // フラグメントシェーダの場合
-        case 'x-shader/x-fragment':
+        case 'fragment-shader':
             shader = gl.createShader(gl.FRAGMENT_SHADER);
             break;
         default :
@@ -99,7 +115,7 @@ function create_shader(id){
     }
 }
 
-function create_program(vs, fs){
+function create_program(gl, vs, fs){
     
     // プログラムオブジェクトの生成
     const program = gl.createProgram();
@@ -124,3 +140,25 @@ function create_program(vs, fs){
         alert(gl.getProgramInfoLog(program));
     }
 }
+
+function createTexture(gl, sizeX, sizeY, internalFormat, format, type) {
+    const texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, internalFormat, sizeX, sizeY, 0, format, type, null);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.bindTexture(gl.TEXTURE_2D, null);
+    return texture;
+}
+
+function setUniformTexture(gl, index, texture, location) {
+    gl.activeTexture(gl.TEXTURE0 + index);
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.uniform1i(location, index);
+}
+
+function setUniformTexture(gl, index, texture, location) {
+    gl.activeTexture(gl.TEXTURE0 + index);
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.uniform1i(location, index);
+  }
