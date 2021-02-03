1ce1a2c20c583c01261db249d9cd6e6749b439e7
nekomehako <ngnlkkngsm@yahoo.co.jp>
2021-02-03 18:58:27 +0900

    add texture view

--- script.js
+++ script.js
@@ -18,23 +18,23 @@ window.onload = function(){
     c.addEventListener('mouseup', mouseUp, true);
 
     // webgl2コンテキストを取得
-    const gl = c.getContext('webgl2')
+    gl = c.getContext('webgl2')
 
     //シェーダーのコンパイル等
-    const v_shader = create_shader(gl,"vs");
-    const f_shader = create_shader(gl,"fs");
+    const v_shader = create_shader("vs");
+    const f_shader = create_shader("fs");
     
     // プログラムオブジェクトの生成とリンク
-    const prg = create_program(gl ,v_shader, f_shader);
-
-    //テクスチャ
-    //フレームバッファの作成
-    const framebuffer = gl.createFramebuffer();
-    gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffer)
-    gl.drawBuffers([gl.COLOR_ATTACHMENT0]);
-
-    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
-    gl.bindTexture(gl.TEXTURE_2D, null);
+    const prg = create_program(v_shader, f_shader);
+    
+    // テクスチャを生成
+    let texture;
+    // イメージオブジェクトの生成
+    let img = new Image();
+    // データのオンロードをトリガーにする
+    img.onload = function(){texture = create_texture(img);};
+    // イメージオブジェクトのソースを指定
+    img.src = "texture.png";
 
     // 頂点情報の設定
     const VERTEX_SIZE = 3
@@ -70,16 +70,22 @@ window.onload = function(){
         gl.clearDepth(1.0);
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
         //uniform vec2 resolution;
-        gl.uniform2f(gl.getUniformLocation(prg, 'resolution'), CANVAS_SIZE_X, CANVAS_SIZE_Y);
+        gl.uniform2f(gl.getUniformLocation(prg, 'resolution'), c.width, c.height);
         //uniform vec2 omouse;
-        gl.uniform2f(gl.getUniformLocation(prg, 'omouse'), omouseX/CANVAS_SIZE_X, omouseY/CANVAS_SIZE_Y);
+        gl.uniform2f(gl.getUniformLocation(prg, 'omouse'), omouseX/c.width, omouseY/c.height);
         //uniform vec2 mouse;
-        gl.uniform2f(gl.getUniformLocation(prg, 'mouse'), mouseX/CANVAS_SIZE_X, mouseY/CANVAS_SIZE_Y);
-        //uniform vec2 mousePress;
+        gl.uniform2f(gl.getUniformLocation(prg, 'mouse'), mouseX/c.width, mouseY/c.height);
+        //uniform float mousePress;
         gl.uniform1f(gl.getUniformLocation(prg, 'mousePress'), mousePress);
+        //uniform float mousePress;
+        gl.uniform1f(gl.getUniformLocation(prg, 'time'), time);
+        //uniform sampler2D previous;
+        setUniformTexture(gl.getUniformLocation(prg, 'previous'), 0, texture);
+
         //頂点の描画 gpuの起動
         gl.drawElements(gl.TRIANGLES, indices.length , gl.UNSIGNED_SHORT, 0);
         gl.flush();
+        time++;
         //再帰する
         setTimeout(arguments.callee, fps);
     })();
@@ -87,9 +93,8 @@ window.onload = function(){
 //マウス座標取得関数
 function mouseMove(e){
     omouseX = mouseX; omouseY = mouseY;
-    const rect = e.target.getBoundingClientRect();
-    mouseX = e.offsetX-rect.left;
-    mouseY = e.offsetY-rect.top;
+    mouseX = e.offsetX;
+    mouseY = e.offsetY;
 }
 
 //マウスがクリックされたときの関数
@@ -101,7 +106,7 @@ function mouseUp(e){
     mousePress = 0;
 }
 //シェーダーのコンパイル関数
-function create_shader(gl, id){
+function create_shader(id){
     // シェーダを格納する変数
     let shader;
     // HTMLからscriptタグへの参照を取得
@@ -136,9 +141,7 @@ function create_shader(gl, id){
         alert(gl.getShaderInfoLog(shader));
     }
 }
-
-function create_program(gl, vs, fs){
-    
+function create_program(vs, fs){
     // プログラムオブジェクトの生成
     const program = gl.createProgram();
     // プログラムオブジェクトにシェーダを割り当てる
@@ -163,17 +166,20 @@ function create_program(gl, vs, fs){
     }
 }
 //テクスチャの生成ヘルパー関数
-function createTexture(gl, sizeX, sizeY, internalFormat, format, type) {
+function create_texture(img) {
     const texture = gl.createTexture();
+    // テクスチャをバインドする
     gl.bindTexture(gl.TEXTURE_2D, texture);
-    gl.texImage2D(gl.TEXTURE_2D, 0, internalFormat, sizeX, sizeY, 0, format, type, null);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    // テクスチャへイメージを適用
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
+    // ミップマップを生成
+    gl.generateMipmap(gl.TEXTURE_2D);
+    // テクスチャのバインドを無効化
     gl.bindTexture(gl.TEXTURE_2D, null);
     return texture;
 }
 //テクスチャをuniform変数に割り当てる
-function setUniformTexture(gl, index, texture, location) {
+function setUniformTexture(location, index, texture) {
     gl.activeTexture(gl.TEXTURE0 + index);
     gl.bindTexture(gl.TEXTURE_2D, texture);
     gl.uniform1i(location, index);
